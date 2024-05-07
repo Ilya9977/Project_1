17c699c6779bedf48f7a1b8663843690d103b663
Ingin <ilatyrtycko@gmail.com>
2024-05-07 17:23:36 +0300

    Задача про координаты

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def f(x, y):
+    if x > y and y > x:
+        print('I')
+    elif x > 0 and y < 0:
+        print('II')
+    elif x < 0 and y > 0:
+        print('III')
+    else:
+        print('IV')
